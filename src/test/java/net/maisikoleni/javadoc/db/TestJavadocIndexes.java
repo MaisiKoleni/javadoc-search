5376c3efd68ce678ddcfb2519611a4da8ddaa52a
Christian Femers <christian.femers@t-online.de>
2022-07-05 23:10:22 +0200

    Create non-persisted JavadocIndexes implementation for testing

---
+++ src/test/java/net/maisikoleni/javadoc/db/TestJavadocIndexes.java
@@ -0,0 +1,22 @@
+package net.maisikoleni.javadoc.db;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
+
+import net.maisikoleni.javadoc.entities.JavadocIndex;
+
+public final class TestJavadocIndexes implements JavadocIndexes {
+
+	private Map<URI, JavadocIndex> indexesByUri = new ConcurrentHashMap<>();
+
+	@Override
+	public JavadocIndex getIndexByBaseUrl(URI baseUrl, Supplier<JavadocIndex> alternativeSource) {
+		return indexesByUri.computeIfAbsent(baseUrl, url -> alternativeSource.get());
+	}
+
+	public Map<URI, JavadocIndex> getRawIndexesByUriMap() {
+		return indexesByUri;
+	}
+}
