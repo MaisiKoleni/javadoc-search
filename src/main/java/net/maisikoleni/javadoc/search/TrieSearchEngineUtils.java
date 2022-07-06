3083308ae2e451f148ae9bf6bccf0e23acfdda8e
Christian Femers <christian.femers@t-online.de>
2022-07-06 23:39:47 +0200

    Fix issues with the skip char in the search regex generation

--- src/main/java/net/maisikoleni/javadoc/search/TrieSearchEngineUtils.java
+++ src/main/java/net/maisikoleni/javadoc/search/TrieSearchEngineUtils.java
@@ -18,13 +18,12 @@ public final class TrieSearchEngineUtils {
 	private static final String SKIP_CHAR = "~";
 	private static final String SEPARATOR_CHAR_CLASS = ".,()<>/\\[\\]";
 	private static final Pattern SEPARATORS = Pattern.compile("[" + SEPARATOR_CHAR_CLASS + "]");
-	private static final Pattern QUERY_SPLIT = Pattern
-			.compile("(?<!" + SKIP_CHAR + ")\\b(?!" + SKIP_CHAR + ")|(?<=[" + SEPARATOR_CHAR_CLASS + "])");
+	private static final Pattern QUERY_SPLIT = Pattern.compile("(?= )|(?<=[" + SEPARATOR_CHAR_CLASS + " ])");
 	private static final Pattern IDENTIFIER_SPLIT = Pattern.compile("(?=[\\p{javaUpperCase}_])");
 	private static final Pattern SKIP_TO_CHAR = Pattern.compile(SKIP_CHAR + "++");
 
 	private static final Pattern QUERY_WHITESPACE = Pattern.compile("\\s++");
-	private static final Pattern QUERY_UNECESSARY_SPACE = Pattern.compile(" \\B|\\B ");
+	private static final Pattern QUERY_UNECESSARY_SPACE = Pattern.compile(" \\B(?!~)|\\B(?<!~) ");
 
 	private static final Regex REGEX_WHITESPACE = new CharClass(caseIndependent(Character::isWhitespace), "\\s");
 	private static final Regex REGEX_ANY_WHITESPACE = new Star(REGEX_WHITESPACE);
@@ -60,32 +59,16 @@ public final class TrieSearchEngineUtils {
 		return false;
 	}
 
-	static <T extends SearchableEntity> void subdivideEntity(T se, SubdividedEntityConsumer<T> consumer) {
-		var preProcessedName = preProcessEntry(se.qualifiedName());
-		consumer.accept(preProcessedName, se, 3.0);
-		int length = preProcessedName.length();
-		for (int i = 1; i < length; i++) {
-			double rank = 0.0;
-			if (REGEX_DIVIDER.matches(preProcessedName, i - 1, i) || REGEX_DIVIDER.matches(preProcessedName, i, i + 1))
-				rank = 2.5;
-			else if (REGEX_START_AFTER.matches(preProcessedName, i - 1, i)
-					|| REGEX_START_BEFORE.matches(preProcessedName, i, i + 1))
-				rank = 1.25;
-			if (rank > 0.0) {
-				var namePart = preProcessedName.subSequence(i, length);
-				if (isUseful(namePart))
-					consumer.accept(namePart, se, rank);
-			}
-		}
-	}
-
-	@FunctionalInterface
-	interface SubdividedEntityConsumer<T> {
-		void accept(CharSequence name, T entity, double rank);
-	}
-
 	static boolean isUseful(CharSequence cs) {
 		return !cs.isEmpty() && (cs.length() > 2 || REGEX_USEFUL_CHARS.matches(cs));
+	}
+
+	static Regex generateRegexFromQuery(String query) {
+		var cleanQuery = preProcessEntry(query.strip());
+		// TODO: keep?
+		if (cleanQuery.toString().endsWith("^^"))
+			cleanQuery = cleanQuery.subSequence(0, cleanQuery.length() - 2).toString().toUpperCase(Locale.ROOT);
+		return generateRegex(cleanQuery);
 	}
 
 	static CharSequence preProcessEntry(CharSequence query) {
@@ -94,7 +77,7 @@ public final class TrieSearchEngineUtils {
 	}
 
 	static Regex generateRegex(CharSequence cleanQuery) {
-		if (cleanQuery.isEmpty() || cleanQuery.length() == 1 && isSeparator(cleanQuery.charAt(0)))
+		if (isInsufficientQuery(cleanQuery))
 			return new Concatenation();
 		return Concatenation.of(Stream.of(QUERY_SPLIT.split(cleanQuery)).map(String::strip).map(part -> {
 			// preserve "significant" whitespace between words
@@ -117,11 +100,32 @@ public final class TrieSearchEngineUtils {
 				REGEX_ANY_NON_DIVIDER);
 	}
 
-	static Regex generateRegexFromQuery(String query) {
-		var cleanQuery = preProcessEntry(query.strip());
-		// TODO: keep?
-		if (cleanQuery.toString().endsWith("^^"))
-			cleanQuery = cleanQuery.subSequence(0, cleanQuery.length() - 2).toString().toUpperCase(Locale.ROOT);
-		return generateRegex(cleanQuery);
+	static boolean isInsufficientQuery(CharSequence cleanQuery) {
+		return cleanQuery.isEmpty() || cleanQuery.length() == 1
+				&& (isSeparator(cleanQuery.charAt(0)) || SKIP_CHAR.charAt(0) == cleanQuery.charAt(0));
+	}
+
+	static <T extends SearchableEntity> void subdivideEntity(T se, SubdividedEntityConsumer<T> consumer) {
+		var preProcessedName = preProcessEntry(se.qualifiedName());
+		consumer.accept(preProcessedName, se, 3.0);
+		int length = preProcessedName.length();
+		for (int i = 1; i < length; i++) {
+			double rank = 0.0;
+			if (REGEX_DIVIDER.matches(preProcessedName, i - 1, i) || REGEX_DIVIDER.matches(preProcessedName, i, i + 1))
+				rank = 2.5;
+			else if (REGEX_START_AFTER.matches(preProcessedName, i - 1, i)
+					|| REGEX_START_BEFORE.matches(preProcessedName, i, i + 1))
+				rank = 1.25;
+			if (rank > 0.0) {
+				var namePart = preProcessedName.subSequence(i, length);
+				if (isUseful(namePart))
+					consumer.accept(namePart, se, rank);
+			}
+		}
+	}
+
+	@FunctionalInterface
+	interface SubdividedEntityConsumer<T> {
+		void accept(CharSequence name, T entity, double rank);
 	}
 }
