0b46880a56ad3331443fdc669e92b96e2cd1a222
Christian Femers <christian.femers@t-online.de>
2022-07-12 05:33:57 +0200

    Add more and better tests for the search API

--- src/test/java/net/maisikoleni/javadoc/server/SearchResourceTest.java
+++ src/test/java/net/maisikoleni/javadoc/server/SearchResourceTest.java
@@ -29,7 +29,14 @@ class SearchResourceTest {
 	}
 
 	@Test
-	void testSuggestions() {
+	void testSearchAndRedirectEmpty() {
+		given().redirects().follow(false).when().get("api/search/redirect?query=").then() //
+				.statusCode(Status.SEE_OTHER.getStatusCode()) //
+				.header(HttpHeaders.LOCATION, "https://docs.oracle.com/en/java/javase/18/docs/api/");
+	}
+
+	@Test
+	void testSuggestionsFound() {
 		given().when().get("api/search/suggestions?query=str Col~or").then() //
 				.statusCode(Status.OK.getStatusCode()) //
 				.body(is("""
@@ -37,5 +44,66 @@ class SearchResourceTest {
 						"java.base/java.util.stream.Collector",\
 						"java.base/java.util.stream.Collectors"\
 						]]"""));
+	}
+
+	@Test
+	void testSuggestionsEmpty() {
+		given().when().get("api/search/suggestions?query=").then() //
+				.statusCode(Status.OK.getStatusCode()) //
+				.body(is("""
+						["",[\
+						]]"""));
+	}
+
+	@Test
+	void testSuggestionsWithCount() {
+		given().when().get("api/search/suggestions?query=str Col~or&count=1").then() //
+				.statusCode(Status.OK.getStatusCode()) //
+				.body(is("""
+						["str Col~or",[\
+						"java.base/java.util.stream.Collector"\
+						]]"""));
+	}
+
+	@Test
+	void testRedirectNoQuery() {
+		given().when().get("api/search/redirect").then() //
+				.statusCode(Status.BAD_REQUEST.getStatusCode()) //
+				.body(is(""));
+	}
+
+	@Test
+	void testRedirectLongQuery() {
+		given().when().get("api/search/redirect?query=" + "a".repeat(1001)).then() //
+				.statusCode(Status.BAD_REQUEST.getStatusCode()) //
+				.body(is(""));
+	}
+
+	@Test
+	void testSuggestionsNoQuery() {
+		given().when().get("api/search/suggestions").then() //
+				.statusCode(Status.BAD_REQUEST.getStatusCode()) //
+				.body(is(""));
+	}
+
+	@Test
+	void testSuggestionsLongQuery() {
+		given().when().get("api/search/suggestions?query=" + "a".repeat(1001)).then() //
+				.statusCode(Status.BAD_REQUEST.getStatusCode()) //
+				.body(is(""));
+	}
+
+	@Test
+	void testSuggestionsLargeCount() {
+		given().when().get("api/search/suggestions?query=Se&count=100").then() //
+				.statusCode(Status.BAD_REQUEST.getStatusCode()) //
+				.body(is(""));
+	}
+
+	@Test
+	void testSuggestionsNegativeCount() {
+		given().when().get("api/search/suggestions?query=Se&count=0").then() //
+				.statusCode(Status.BAD_REQUEST.getStatusCode()) //
+				.body(is(""));
 	}
 }
