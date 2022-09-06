4cd557b0623b3488545b411262962f1c9cb33300
Christian Femers <christian.femers@t-online.de>
2022-09-06 03:15:57 +0200

    Fix and finish tests and API migration as well as library serialization

---
+++ src/test/java/net/maisikoleni/javadoc/server/LegacyApiV1SearchResourceTest.java
@@ -0,0 +1,37 @@
+package net.maisikoleni.javadoc.server;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response.Status;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class LegacyApiV1SearchResourceTest {
+
+	private static final String ROUTE_PREFIX = "/api/search/";
+
+	@Test
+	void testSearchAndRedirectFound() {
+		given().redirects().follow(false).when().get(ROUTE_PREFIX + "redirect?query=str Col~or").then() //
+				.body(is("")) //
+				.statusCode(Status.SEE_OTHER.getStatusCode()) //
+				.header(HttpHeaders.LOCATION,
+						"https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/stream/Collector.html");
+	}
+
+	@Test
+	void testSuggestionsFound() {
+		given().when().get(ROUTE_PREFIX + "suggestions?query=str Col~or").then() //
+				.statusCode(Status.OK.getStatusCode()) //
+				.body(is("""
+						["str Col~or",[\
+						"java.base/java.util.stream.Collector",\
+						"java.base/java.util.stream.Collectors"\
+						]]"""));
+	}
+}
---
+++ src/test/java/net/maisikoleni/javadoc/server/LibraryResourceTest.java
@@ -0,0 +1,35 @@
+package net.maisikoleni.javadoc.server;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class LibraryResourceTest {
+
+	private static final String ROUTE_PREFIX = "/api/v2/libraries";
+
+	@Test
+	void testGetAll() {
+		given().when().get(ROUTE_PREFIX).then() //
+				.statusCode(Status.OK.getStatusCode()) //
+				.body(is("""
+						[{\
+						"name":"JDK 18",\
+						"id":"jdk-latest",\
+						"description":"Latest JDK Release",\
+						"baseUrl":"https://docs.oracle.com/en/java/javase/18/docs/api/"\
+						},{\
+						"name":"JUnit 5",\
+						"id":"junit5-latest",\
+						"description":"Current JUnit 5",\
+						"baseUrl":"https://junit.org/junit5/docs/current/api/"\
+						}]\
+						"""));
+	}
+}
